be841c5bf40b3d6957ab21fb6f087b7399c05eb9
1nn0vat0r <bsw8187@naver.com>
2022-08-02 14:00:05 +0000

    3rd build

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -25,8 +25,50 @@ const About = () => {
 
   return (
     <>
-      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
-
+      <div className="container px-4 px-lg-5">
+        <h2 className="text-center about mt-0">About Me</h2>
+        <hr className="divider" />
+        <div className="row gx-3 gx-lg-5">
+          <div className="col-lg-4 col-md-4 text-center">
+            <div className="mt-5">
+              <h3 className="h4 mb-2">Name</h3>
+              <p className="text-muted mb-0">Bae Seung Woo, 1nn0vat0r</p>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 text-center">
+            <div className="mt-5">
+              <h3 className="h4 mb-2">Education</h3>
+              <p className="text-muted mb-0">SungKyunKwan University</p>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 text-center">
+            <div className="mt-5">
+              <h3 className="h4 mb-2">Contact</h3>
+              <p className="text-muted mb-0">+82 010-3809-8122</p>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 text-center">
+            <div className="mt-5">
+              <h3 className="h4 mb-2">E-mail</h3>
+              <p className="text-muted mb-0">bsw8187@naver.com</p>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 text-center">
+            <div className="mt-5">
+              <h3 className="h4 mb-2">My Address</h3>
+              <p className="text-muted mb-0">Seoul, South Korea</p>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 text-center">
+            <div className="mt-5">
+              <h3 className="h4 mb-2">Date of Birth</h3>
+              <p className="text-muted mb-0">1999.09.17</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
